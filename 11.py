8ef526be98e7ee440f75bdc8980279276a7f0443
gjrbrosula <gabrielbrosula@gmail.com>
2024-12-11 21:06:35 -0500

    Revert "Attempt to use log space to solve the problem"
    This reverts commit 6595f2684a410fc6c0b1f6f7e12f88d8503216d6.

--- 11.py
+++ 11.py
@@ -1,5 +1,4 @@
 from tqdm import tqdm
-from math import log10, floor
 
 day = "11"
 test = f"{day}-small"
@@ -8,59 +7,28 @@ real = f"{day}"
 
 input = []
 
-with open(f'./input/{test}.txt', 'r') as f:
+with open(f'./input/{real}.txt', 'r') as f:
+
     line = f.readline()
-    input = [int(x) for x in line.split(' ')]
+    input = line.split(' ')
 
 # Read from input
 # Copy into new_list
-
-# Part 2: Use log space
-for i in range(len(input)):
-    if input[i] != 0:
-        input[i] = log10(input[i])
-    else:
-        input[i] = -1
-
-log_2024 = log10(2024)
-def get_num_digits(num):
-    if num < 0:
-        return 1
-    return floor(num) + 1
-
-for i in tqdm(range(6)):
+for i in tqdm(range(75)):
     new_list = []
-    print(f"Blink {i+1}")
     for j in range(len(input)):
-        lognum = input[j]
-
-        # Using negative 1 to indicate that the previous number is a 0
-        if lognum == -1:
-            # 0 turns into a 1
-            # log(1) = 0
-            new_list.append(0)
+        ns = str(int(input[j]))
+        if (ns == "0"):
+            new_list.append("1")
+        elif (len(ns) % 2 == 0):
+            mp = len(ns) // 2
+            lh = ns[:mp]
+            rh = ns[mp:]
+            new_list.append(lh)
+            new_list.append(rh)
         else:
-            if get_num_digits(lognum) % 2 == 0:
-                # Turn to string
-                actual = round(pow(10, lognum))
-                factor = pow(10, get_num_digits(lognum) - 1)
-                lh_num = actual // factor
-                rh_num = actual % factor
+            new_list.append(str(int(ns) * 2024))
 
-                print(f"actual: {actual}, factor: {factor}, lh: {lh_num}, rh: {rh_num}")
-
-                # actual = 4412 2351
-                # 4412 = actual // 10,000
-                # 2351 = actual % 10,000
-
-                new_list.append(log10(lh_num) if lh_num != 0 else -1)
-                new_list.append(log10(rh_num) if rh_num != 0 else -1)
-            else:
-                new_list.append(log_2024 + lognum)
-                print(pow(10, log_2024 + lognum))
     input = new_list
-    print([round(pow(10, x)) for x in input])
-    print(input)
 
-print([get_num_digits(x) for x in input])
-print(sum([get_num_digits(x) for x in input]))
+print(len(input))
